6daf02ee228d175801c534ac82c52aa09c45da85
vsemenyakin <vsemenyakinD@yandex.ru>
2016-02-18 02:05:38 +0200

    Added static linked assimp library. IT WORKS, MAN!

--- opengl_test/src/opengl_test.cpp
+++ opengl_test/src/opengl_test.cpp
@@ -15,6 +15,9 @@ Comment:  од чесно скопирован с того самого учебника на wikibooks
 
 #include <math.h>
 
+#include <assimp/Importer.hpp>      // C++ importer interface
+#include <assimp/scene.h>           // Output data structure
+#include <assimp/postprocess.h>     // Post processing flags
 
 #define _USE_MATH_DEFINES //In the name of Great and Powerful PI
 
@@ -34,6 +37,38 @@ Comment:  од чесно скопирован с того самого учебника на wikibooks
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+
+
+
+
+
+
+
+//----------------------------------------------------------
+bool DoTheImportThing( const std::string& pFile)
+{
+  // Create an instance of the Importer class
+  Assimp::Importer importer;
+  // And have it read the given file with some example postprocessing
+  // Usually - if speed is not the most important aspect for you - you'll
+  // propably to request more postprocessing than we do in this example.
+  const aiScene* scene = importer.ReadFile( pFile,
+        aiProcess_CalcTangentSpace       |
+        aiProcess_Triangulate            |
+        aiProcess_JoinIdenticalVertices  |
+        aiProcess_SortByPType);
+
+  // If the import failed, report it
+  if( !scene)
+  {
+//    DoTheErrorLogging( importer.GetErrorString());
+    return false;
+  }
+  // Now we can access the file's contents.
+//  DoTheSceneProcessing( scene);
+  // We're done. Everything will be cleaned up by the importer destructor
+  return true;
+}
 
 //Screen properties
 //----------------------------------------------------------
@@ -332,6 +367,8 @@ void free_resources() {
 //=============================================================================
 int main(int argc, char* argv[])
 {
+	DoTheImportThing(std::string("test_path.txt"));
+
 	glutInit(&argc, argv);
 	//	glutInitContextVersion(2,0);
 
